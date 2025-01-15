4f4e2f543f2c2e3d9dc4e946721caac4904fc671
GalacticRe <dwlow@ualberta.ca>
2025-01-15 15:47:18 -0600

    Adds functionality for add and delete button

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.listycity;
 
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
-    Tex
+    Button add_button;
+    Button delete_button;
+    TextInputEditText text_in;
+    int selected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +36,46 @@ public class MainActivity extends AppCompatActivity {
         dataList.addAll(Arrays.asList(cities));
         cityAdapter = new ArrayAdapter<>(this,R.layout.content,dataList);
         cityList.setAdapter(cityAdapter);
+        // Gets all my newly created buttons and text input
+        delete_button = findViewById(R.id.delete_button);
+        add_button = findViewById(R.id.add_button);
+        text_in = findViewById(R.id.city_name_input);
+        // Creates a default selected item
+        selected = -1;
+        // Makes the program store the selected item
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected = position;
+            }
+        });
+        // Adds the button functionality
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Gets the hint message
+                CharSequence text_string;
+                text_string = text_in.getText();
+                assert text_string != null;
+                // Prevents empty string from being added
+                if (!text_string.toString().isEmpty()) {
+                    // Adds it to the list
+                    dataList.add(text_string.toString());
+                    // Notifies the adapter to change the screen
+                    cityAdapter.notifyDataSetChanged();}
+            }
+        });
+        // Creates the button action to remove an item
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ensures all results are valid
+                if (selected != -1){
+                    // Gets the item and removes it from the list
+                    dataList.remove(selected);
+                    selected = -1;
+                    cityAdapter.notifyDataSetChanged();}
+            }
+        });
     }
 }
